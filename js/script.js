074ad5eece7a87bcad4a937fc511d355ecdd9ae5
SilviaDiniz <sreginardiniz@hotmail.com>
2018-08-09 15:11:19 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $(document).ready(function() {
 
 
 });
+
+
+function jsonThen(func) {
+  return function (response) {
+    response.json().then(func);
+  }
+}
+
+function myMethod(json) {
+  //coloque aqui dentro o seu código
+  console.log(json);
+}
+
+
+fetch('https://api.deezer.com/search/track?q=soundtrack').then(jsonThen(myMethod));
